7bb2b552b22f7b79edb87016a1e442f1cb855292
Abhinav Kumar <abhinavkumar0605@gmail.com>
2026-06-02 03:08:15 +0530

    js practice

---
+++ learn/js/practice1.js
@@ -0,0 +1,50 @@
+// sum of two Number
+
+// let a=10;
+// let b=34;
+
+// console.log("sum:",a+b);
+
+// function sum(a,b){
+//     return a+b;
+// }
+
+// let c=sum(6,9);
+// console.log(c)
+
+
+
+
+// Greeting generator
+
+// let name="Abhi";
+
+// console.log("hlw "+name);
+
+// function greet(name){
+//     console.log("hlw "+name);
+
+// }
+
+// greet("Abhi")
+
+
+// Calculator using arrow functions
+
+// let add=(a,b)=>{
+//     return a+b;
+// }
+// let sub=(a,b)=>{
+//     return a-b;
+// }
+// let mul=(a,b)=>{
+//     return a*b;
+// }
+// let div=(a,b)=>{
+//     return a/b;
+// }
+
+// console.log(add(8,6));
+// console.log(sub(8,6));
+// console.log(mul(8,6));
+// console.log(div(8,6));
