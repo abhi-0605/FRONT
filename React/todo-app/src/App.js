6b72bcb30e92694a60060c9b7fdbe1cf5d9387f8
Abhinav Kumar <abhinavkumar0605.work@gmail.com>
2026-06-29 00:37:16 +0530

    props

--- React/todo-app/src/App.js
+++ React/todo-app/src/App.js
@@ -1,26 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './my-component/Header';
+import {Todoitem} from './my-component/Todoitem';
+import {Todos} from './my-component/Todos';
+import {Footer} from './my-component/Footer';
+
 
 function App() {
+  const ondelete =(todo)=>{
+    console.log("i am ondelete of todo",todo)
+    let index=todos.indexOf(todo);
+    todos.splice(index,1);
+  }
+  let todos=[
+    {
+      sno:1,
+      title: "Go to market",       
+      desc: "you need to go to market"
+    },
+    {
+      sno:2,
+      title: "Go to mall",
+      desc: "you need to go to market"
+    },
+    {
+      sno:3,
+      title: "Go to ghat",
+      desc: "you need to go to market"
+    },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="MyTodoList" searchbar={false}/>
+      <Todos todos={todos} ondelete={ondelete}/>
+      <Footer/>
+    </>
   );
 }
+
 
 export default App;
