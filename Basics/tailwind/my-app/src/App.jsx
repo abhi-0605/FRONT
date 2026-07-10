0f54f58cfcebe8e1dd732d20906cb18d3ccee1e9
Abhinav Kumar <abhinavkumar0605.work@gmail.com>
2026-07-11 00:47:50 +0530

    tailwind CSS

---
+++ Basics/tailwind/my-app/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <nav className="bg-gray-600 text-white flex md:justify-between flex-col md:flex-row min-h-15" >
+        <div className="logo mx-auto md:mx-0" >Logo</div>
+        <ul className="flex space-x-3 justify-center md:normal-case">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+    </>
+  )
+}
+
+export default App
